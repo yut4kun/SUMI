195d8dfc98f258236b7514ef26708675206a0474
Yuta kun <63550914+yut4kun@users.noreply.github.com>
2022-12-07 22:02:11 +0600

    Update config.py

--- SUMI/config.py
+++ SUMI/config.py
@@ -72,6 +72,7 @@ class Config(object):
     BOT_USERNAME = "Albedo_AliasRobot" #Your bot username without @.
     BOT_NAME = "Albedo" #Your bot Name.
     REDIS_URL = "" #Paste your redis url in format redis://<username of admin role>:<password of admin role>@<endoint url>/<database name>. YOU CAN SKIP THIS IF YOU DONT HAVE ONE
+    ARQ_API_URL = ""
     ARQ_API_KEY = "HZULWN-XDDTFD-OISRZZ-MPHDOR-ARQ" #For Some Modules. Get it from @ArqRobot
 
 #Damn, it was tough for me to rewrite this whole, please don't remove the credits from the code, i did too much hardwork for this
